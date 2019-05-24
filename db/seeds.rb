36c908b11ccaed47739a38c0fe67cd0c2c9a0f67
joyceyskwon <joyceyskwon@gmail.com>
2019-05-24 18:22:28 -0400

    pulling data from san francisco

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 require 'json'
 
 # Get request to the API
-breweries_string = RestClient.get('https://api.openbrewerydb.org/breweries?by_city=new_york')
+breweries_string = RestClient.get('https://api.openbrewerydb.org/breweries?by_city=san_francisco')
 breweries_hash = JSON.parse(breweries_string)
 
 # Iterate through each show from API and create data for the rails api backend
